4f581c89209b4e74cef1d5266011c55b4944113b
rosha <rosha@DESKTOP-4B0L93B>
2026-02-24 14:09:29 +0530

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of MVC",
+                DateOfBirth = new DateTime(1954,12,1)
+            };
+            return View(data);
+        }
+    }
+}
